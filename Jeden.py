c3e01ea3ff6e13534fb3c641eca4409c056f5988
ksiciarz1 <kacpersiciarz2001@gmail.com>
2023-03-25 14:36:01 +0100

    Dokończono selektory aby mieć prawidłowe i czytelne dane

--- Jeden.py
+++ Jeden.py
@@ -28,18 +28,22 @@ while errorLoop == "Y":
             print("Są opinie")
             reviewList = []
             for review in reviews:
+                pros = review.select("div.review-feature__title.review-feature__title--positives")[0].parent.contents if len(review.select("div.review-feature__title.review-feature__title--positives")) > 0 else []
+                cons = review.select("div.review-feature__title.review-feature__title--negatives")[0].parent.contents if (len(review.select("div.review-feature__title.review-feature__title--negatives")) > 0) else []
+                
+                
                 myReview = {
                     "id": review.get("data-entry-id"),
-                    'author': review.select("div.user-post__content > span.user-post__author-name")[0].contents,
-                    "recomendation": review.select("div.user-post__content > span.user-post__author-recomendation > em")[0].contents if len(review.select("div.user-post__content > span.user-post__author-recomendation > em")) > 0 else -1,
-                    "stars": review.select("div.user-post__content > span.user-post__score > span.user-post__score-count")[0].contents,
-                    "trustedReview": review.select("#reviews > div > div.js_product-reviews.js_reviews-hook.js_product-reviews-container > div:nth-child(1) > div > div.user-post__info > div.review-pz")[0] if (len(review.select("#reviews > div > div.js_product-reviews.js_reviews-hook.js_product-reviews-container > div:nth-child(1) > div > div.user-post__info > div.review-pz")) > 0) else -1,
+                    'author': review.select("div.user-post__content > span.user-post__author-name")[0].text,
+                    "recomendation": review.select("div.user-post__content > span.user-post__author-recomendation > em")[0].text if len(review.select("div.user-post__content > span.user-post__author-recomendation > em")) > 0 else -1,
+                    "stars": review.select("div.user-post__content > span.user-post__score > span.user-post__score-count")[0].text,
+                    "trustedReview": True if (len(review.select("div.user-post__info > div.review-pz")) > 0) else False,
                     "date": review.select("div.user-post__content > span.user-post__score > span.user-post__published > time:nth-child(1)")[0].get('datetime'),
                     "purchasedDate": review.select("div.user-post__content > span.user-post__score > span.user-post__published > time:nth-child(2)")[0].get('datetime'),
-                    "usefull": review.select("div.user-post__info > div.js_product-review-usefulness.vote > button.vote-yes.js_product-review-vote.js_vote-yes")[0].contents,
-                    "notUsefull": review.select("div.user-post__info > div.js_product-review-usefulness.vote > button.vote-no.js_product-review-vote.js_vote-no")[0].contents,
-                    "content": review.select("div.user-post__content > div.user-post__text")[0].contents,
-                    "prosCount": len(review.select("div.review-feature__title.review-feature__title--positives")[0].parent.children) - 1 if (len(review.select("div.review-feature__title.review-feature__title--positives")) > 0) else -1,
-                    "consCount": len(review.select("div.review-feature__title.review-feature__title--negatives")[0].parent.children) - 1 if (len(review.select("div.review-feature__title.review-feature__title--negatives")) > 0) else -1
+                    "usefull": review.select("div.user-post__info > div.js_product-review-usefulness.vote > button.vote-yes.js_product-review-vote.js_vote-yes")[0].text,
+                    "notUsefull": review.select("div.user-post__info > div.js_product-review-usefulness.vote > button.vote-no.js_product-review-vote.js_vote-no")[0].text,
+                    "content": review.select("div.user-post__content > div.user-post__text")[0].text,
+                    "prosCount": len(list(filter(lambda pro: pro != "\n", pros))) - 1 if len(pros) > 0 else -1,
+                    "consCount": len(list(filter(lambda con: con != "\n", cons))) - 1 if len(cons) > 0 else -1
                 }
                 print(review)
