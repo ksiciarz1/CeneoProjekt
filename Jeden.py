ba4813ae24c2df9ac4d00708e69f4066785962e7
ksiciarz1 <kacpersiciarz2001@gmail.com>
2023-05-12 08:04:14 +0200

    Nadgonienie ostatniej nieobecności

--- Jeden.py
+++ Jeden.py
@@ -1,16 +1,47 @@
 # 144166703
 # 144826098
+# 58835954
 import requests
 from bs4 import BeautifulSoup
+import json
 
+def get_cos(ancestor, selector=None, attribute=None, return_list=False):
+    try:
+        if return_list:
+            return [tag.get_text().strip() for tag in ancestor.select(selector)]
+        if not selector and attribute:
+            return ancestor[attribute].strip()
+        if attribute:
+            return ancestor.select_one(selector)[attribute].strip()
+        return ancestor.select_one(selector).get_text().strip()
+    except (AttributeError, TypeError):
+        return None
+    
+mySelectors = {
+    "opinion_id": (None, "data-entry-id"),
+    "author": ("span.user-post__author-name",),
+    "recommendation": ("span.user-post__author-recomendation > em",),
+    "stars": ("span.user-post__score-count",),
+    "purchased": ("div.review-pz",),
+    "opinion_date": ("span.user-post__published > time:nth-child(1)","datetime"),
+    "purchase_date": ("span.user-post__published > time:nth-child(2)","datetime"),
+    "usefull_count": ("button.vote-yes","data-total-vote"),
+    "unusefull_count": ("button.vote-no","data-total-vote"),
+    "content": ("div.user-post__text",),
+    "pros": ("div.review-feature__title--positives ~ div.review-feature__item", None, True),
+    "cons": ("div.review-feature__title--negatives ~ div.review-feature__item", None, True)
+}
+                
 errorLoop = "Y"
 while errorLoop == "Y":
+    errorLoop = ""
+    
     product_id = input("Podaj kod produktu:\n")
+    product_id = 58835954
     url = f"https://www.ceneo.pl/{product_id}#tab=reviews"
 
     # Getting html
     site = requests.get(url)
-    errorLoop = ""
     if (not site.status_code == requests.codes.ok):
         print(f"Error {site.status_code}")
         errorLoop = input("Czy chcesz podać inny kod produktu? (Y/N)")
@@ -18,32 +49,18 @@ while errorLoop == "Y":
     else:
         soup = BeautifulSoup(site.content, 'html.parser')
 
-        # user-post user-post__card js_product-review
-        # user-post js_comment-from-user
         reviews = soup.select('div.js_product-review')
         if (not len(reviews) > 0):
             print("Nie ma opini")
             errorLoop = input("Czy chcesz podać inny kod produktu? (Y/N)")
         else:
-            print("Są opinie")
             reviewList = []
             for review in reviews:
-                pros = review.select("div.review-feature__title.review-feature__title--positives")[0].parent.contents if len(review.select("div.review-feature__title.review-feature__title--positives")) > 0 else []
-                cons = review.select("div.review-feature__title.review-feature__title--negatives")[0].parent.contents if (len(review.select("div.review-feature__title.review-feature__title--negatives")) > 0) else []
-                
-                
-                myReview = {
-                    "id": review.get("data-entry-id"),
-                    'author': review.select("div.user-post__content > span.user-post__author-name")[0].text,
-                    "recomendation": review.select("div.user-post__content > span.user-post__author-recomendation > em")[0].text if len(review.select("div.user-post__content > span.user-post__author-recomendation > em")) > 0 else -1,
-                    "stars": review.select("div.user-post__content > span.user-post__score > span.user-post__score-count")[0].text,
-                    "trustedReview": True if (len(review.select("div.user-post__info > div.review-pz")) > 0) else False,
-                    "date": review.select("div.user-post__content > span.user-post__score > span.user-post__published > time:nth-child(1)")[0].get('datetime'),
-                    "purchasedDate": review.select("div.user-post__content > span.user-post__score > span.user-post__published > time:nth-child(2)")[0].get('datetime'),
-                    "usefull": review.select("div.user-post__info > div.js_product-review-usefulness.vote > button.vote-yes.js_product-review-vote.js_vote-yes")[0].text,
-                    "notUsefull": review.select("div.user-post__info > div.js_product-review-usefulness.vote > button.vote-no.js_product-review-vote.js_vote-no")[0].text,
-                    "content": review.select("div.user-post__content > div.user-post__text")[0].text,
-                    "prosCount": len(list(filter(lambda pro: pro != "\n", pros))) - 1 if len(pros) > 0 else -1,
-                    "consCount": len(list(filter(lambda con: con != "\n", cons))) - 1 if len(cons) > 0 else -1
-                }
-                print(review)
+                myReview = {}
+                for key, value in mySelectors.items():
+                    myReview[key] = get_cos(review, *value)
+                reviewList.append(myReview)
+            json_object = json.dumps(reviewList, indent=4)
+            
+            with open("reviews.json", "w") as outfile:
+                outfile.write(json_object)
